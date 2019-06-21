9ee18c70cb28228d035b7e1e6cbee1c0a68ec4ac
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-21 02:57:06 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,8 +32,8 @@ def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
   #movie = Movie.create(attributes)
   #movie
   Movie.create do |m|
-    m.title = "The Room"
-    m.release_date = 2003
+    m.title: "The Room"
+    m.release_date: 2003
   end
 end
 
